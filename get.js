64dc12967e98d7e443f68f073467684bdf0a2269
Jack Roberts <roberts@pnut.com>
2019-11-17 15:11:11 -0500

    The name refactoring continues

--- get.js
+++ get.js
@@ -6,7 +6,7 @@ export async function main(event, context) {
     TableName: process.env.tableName,
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      docId: event.pathParameters.id
     }
   };
 
